d81ee74c97d9a74ec16a0a83ebbdb4285792f127
Angel Barillas <a.e.barillas.ablan@gmail.com>
2025-03-29 13:26:07 -0400

    added vercel.json to reqrite refreshes without crash

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,11 +1,14 @@
+import { Margin } from "@mui/icons-material";
+import { colors } from "@mui/material";
 import React from "react";
 
 const BookingPage = () => {
   const styles = {
     border: "0",
     width: "100%",
-    height: "100vh",
+    height: "80vh",
     frameBorder: "0",
+    marginBottom: "0",
   };
   return (
     <iframe
